3351b2dad9a2e162f03d021b3b72b081d88ceda5
root <root@LAPTOP-73NIFDJ5.>
2025-03-03 19:17:41 +0700

    update pingpong

--- user/pingpong.c
+++ user/pingpong.c
@@ -5,7 +5,7 @@
 int main(int argc, char *argv[]) {
     int p_1[2];
     int p_2[2];
-    char byte = 'A';
+    char byte, output[14] = "received p.ng";
 
     if (pipe(p_1) < 0 || pipe(p_2) < 0) {
         fprintf(2, "pipe creation failed\n");
@@ -26,8 +26,11 @@ int main(int argc, char *argv[]) {
             exit(1);
         }
         close(p_1[0]);
-        printf("%d: received ping\n", getpid());
+        
+        output[10] = byte;
+        printf("%d: %s\n", getpid(), output);
 
+        byte = 'o';
         if (write(p_2[1], &byte, 1) != 1) {
             fprintf(2, "child: write failed\n");
             exit(1);
@@ -38,17 +41,21 @@ int main(int argc, char *argv[]) {
     } else {
         close(p_2[1]);
         close(p_1[0]);
+        byte = 'i';
         if (write(p_1[1], &byte, 1) != 1) {
             fprintf(2, "parent: write failed\n");
             exit(1);
         }
         close(p_1[1]);
+
         wait(0);
         if (read(p_2[0], &byte, 1) != 1) {
             fprintf(2, "parent: read failed\n");
             exit(1);
         }
-        printf("%d: received pong\n", getpid());
+
+        output[10] = byte;
+        printf("%d: %s\n", getpid(), output);
         close(p_2[0]);
         exit(0);
     }

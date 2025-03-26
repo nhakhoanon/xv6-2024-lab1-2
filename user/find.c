f1a191fcd8ed4ef39e30817bfe1eba68b40f8674
Your Name <you@example.com>
2025-03-26 13:22:05 +0700

    add comment to project1

--- user/find.c
+++ user/find.c
@@ -4,25 +4,34 @@
 #include "kernel/fs.h"
 #include "kernel/fcntl.h"
 
+// nhận vào một chuỗi path(đường dẫn đầy đủ) 
+// và trả về con trỏ đến phần tên file nằm ở cuối đường dẫn
+// Giúp trích xuất tên tập tin từ đường dẫn để so sánh với tên đihcs
 char* getFileName(char *path) {
   char *p;
+
+  //Sau khi chạy xong vòng lặp for này, con trỏ sẽ tới dấu / ngay trước tên(nếu có)
   for (p = path+strlen(path); p >= path && *p != '/'; p--);
+  // phải p++ lại để con trỏ trỏ tới ngay vị trí bắt đầu của tên
   p++;
 
   return p;
 }
 
+// đây là hàm chính thực hiện tìm kiếm đệ quy trong cây thư mục/    
 void find(char *path, char *target) {
   char buf[512], *p;
   int fd;
   struct dirent de;
   struct stat st;
 
+  // mở tệp ở chế độ chỉ đọc (O_RDONLY)
   if ((fd = open(path, O_RDONLY)) < 0) {
     fprintf(2, "find: cannot open %s\n", path);
     return;
   }
-
+  // lấy thông tin tệp
+  // đưa thông tin tệp vào struct st
   if (fstat(fd, &st) < 0) {
     fprintf(2, "find: cannot stat %s\n", path);
     close(fd);
@@ -32,23 +41,37 @@ void find(char *path, char *target) {
   switch (st.type) {
     case T_DEVICE:
     case T_FILE:
+        // so sánh tên của tập tin(hoặc thiết bị) với target, nếu trùng tên, in ra đường dẫn
       if (strcmp(getFileName(path), target) == 0) printf("%s\n", path);
       break;
-  
+    // nếu là thư mục: duyệt qua từng entry trong thư mục và gọi đệ quy
+    // hàm find() cho mỗi entry hợp lệ
     case T_DIR:
+      // kiểm tra độ dài đường dẫn.
       if(strlen(path) + 1 + DIRSIZ + 1 > sizeof buf){
         printf("find: path too long\n");
         break;
       }
+      // copy vào char buf[512]
       strcpy(buf, path);
+      //đặt con trỏ p vào cuối chuỗi đường dẫn
       p = buf + strlen(buf);
+      // ghi kí tự '/' vào vị trí bộ nhớ mà p đang trỏ tới(để nối với tập tin/thư mục con)
+      // sau đó, dịch con trỏ p sang vị trí tiếp theo trong bộ nhớ
       *p++ = '/';
       while (read(fd, &de, sizeof(de)) == sizeof(de)) {
+        // bỏ qua entry không hợp lệ
+        //nếu de.inum == 0, entry này không hợp lệ hoặc đã bị xóa
         if (de.inum == 0) continue;
+        // bỏ qua thư mục đặc biệt(".": thư mục hiện tại)
+        // và ("..": thư mục cha) để tránh đệ quy vô hạn
         if (strcmp(de.name, ".") == 0 || strcmp(de.name, "..") == 0) continue;
+        // sao chép tên tập tin/ thư mục từ de.name vào vị trí p trong buf
         memmove(p, de.name, DIRSIZ);
+        // đảm bảo chuỗi kết thúc đúng cách(bằng ký tự \0)
         p[DIRSIZ] = 0;
-        
+        // gọi đệ quy để tiếp tục tìm kiếm trong thư mcuj con
+        // hoặc kiểm tra tập tin con
         find(buf, target);
       }
       break;
--- user/pingpong.c
+++ user/pingpong.c
@@ -3,6 +3,9 @@
 #include "user/user.h"
 
 int main(int argc, char *argv[]) {
+    // đầu đọc là p[0] và đầu ghi là p[1]
+    //p_1 sẽ là pipe để tiến trình cha gửi dữ liệu cho tiến trình con
+    //p_2 sẽ là pipe để tiến trình con gửi dữ liệu cho tiến trình cha
     int p_1[2];
     int p_2[2];
     char byte, output[14] = "received p.ng";
@@ -17,38 +20,53 @@ int main(int argc, char *argv[]) {
         fprintf(2, "fork failed\n");
         exit(1);
     }
-
+    // kiểm tra liệu đây là tiến trình con hay không
     if (pid == 0) {
+        // Con không ghi vào p_1, nên đóng đầu ghi
         close(p_1[1]);
+        // Con không đọc từ p_2, nên đóng đầu đọc
         close(p_2[0]);
+        // kiểm tra xem liệu có đọc được 1 byte từ tiến trình cha hay không, nếu không thì trả về mã thoát bằng 1
+        // ở đây nếu đọc được thì byte = 'i'
         if (read(p_1[0], &byte, 1) != 1) {
             fprintf(2, "child: read failed\n");
             exit(1);
         }
+        // Con đã đọc từ p_1 xong, đóng đầu đọc.
         close(p_1[0]);
         
         output[10] = byte;
         printf("%d: %s\n", getpid(), output);
 
         byte = 'o';
+        // ghi vào p_2 kí tự byte = 'o'
         if (write(p_2[1], &byte, 1) != 1) {
             fprintf(2, "child: write failed\n");
             exit(1);
         }
+        // Cha đã ghi vào p_2 xong, đóng đầu ghi
         close(p_2[1]);
         exit(0);
 
-    } else {
+    } 
+    // nếu đây là tiến trình cha
+    else {
+        // Cha không ghi vào p_2, nên đóng đầu ghi
         close(p_2[1]);
+        //Cha không đọc từ p_1, nên đóng đầu đọc
         close(p_1[0]);
         byte = 'i';
+        // ghi vào p_1 kí tự byte = 'i'
         if (write(p_1[1], &byte, 1) != 1) {
             fprintf(2, "parent: write failed\n");
             exit(1);
         }
+        // Cha đã ghi vào p_1 xong, đóng đầu ghi
         close(p_1[1]);
-
+        // chờ tiến trình con kết thúc
         wait(0);
+        // kiểm tra xem liệu có đọc được 1 byte từ tiến trình con hay không, nếu không thì trả về mã thoát bằng 1
+        // ở đây nếu đọc được thì byte = 'o'
         if (read(p_2[0], &byte, 1) != 1) {
             fprintf(2, "parent: read failed\n");
             exit(1);
@@ -56,6 +74,7 @@ int main(int argc, char *argv[]) {
 
         output[10] = byte;
         printf("%d: %s\n", getpid(), output);
+        // Cha đã đọc từ p_2 xong, đóng đầu đọc.
         close(p_2[0]);
         exit(0);
     }
